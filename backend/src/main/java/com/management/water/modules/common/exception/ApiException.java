62b7ed567c39ee3755ee1bdc9bb2646cbf49ac27
D-Ojier <reijo23124@gmail.com>
2026-06-23 13:17:20 +0530

    improved global exceptions

--- backend/src/main/java/com/management/water/modules/common/exception/ApiException.java
+++ backend/src/main/java/com/management/water/modules/common/exception/ApiException.java
@@ -26,4 +26,35 @@ public class ApiException extends RuntimeException {
   public String getClientMessage() {
     return clientMessage;
   }
+
+  public static class NotFoundException extends ApiException {
+    public NotFoundException(String message) {
+      super(HttpStatus.NOT_FOUND, message);
+    }
+  }
+
+  public static class BadRequestException extends ApiException {
+    public BadRequestException(String message) {
+      super(HttpStatus.BAD_REQUEST, message);
+    }
+  }
+
+  public static class ConflictException extends ApiException {
+    public ConflictException(String message) {
+      super(HttpStatus.CONFLICT, message);
+    }
+  }
+
+  public static class ForbiddenException extends ApiException {
+    public ForbiddenException(String message) {
+      super(HttpStatus.FORBIDDEN, message);
+    }
+  }
+
+  public static class UnauthorizedException extends ApiException {
+    public UnauthorizedException(String message) {
+      super(HttpStatus.UNAUTHORIZED, message);
+    }
+  }
 }
+
